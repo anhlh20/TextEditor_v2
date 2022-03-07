f17f4e6a5864244e7bf0c92665543508e501064b
anhlh <lahonganh.k61hust@gmail.com>
2022-03-07 22:59:29 +0700

    create frame

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,8 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TextEditor extends JFrame implements ActionListener {
+    TextEditor(){
+
+        this.setTitle("Text Editor");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setVisible(true);
+        this.setLayout(new FlowLayout());
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
